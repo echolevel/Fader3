d59e82c2f76f1d36dfc1ae5f8dfd53499c577419
echolevel <echolevel@gmail.com>
2025-01-04 20:39:32 +0000

    docs

--- boot.py
+++ boot.py
@@ -5,6 +5,8 @@ startupkey = digitalio.DigitalInOut(board.GP0)
 startupkey.direction = digitalio.Direction.INPUT
 startupkey.pull = digitalio.Pull.UP
 
+## WORK IN PROGRESS - this is pilfered from my Macro Pad code and I need a better startup sequence
+## this time around. 
 
 # If the top left button isn't held down on startup, disable
 # mass storage, USB HID, and CDC (serial) modes
